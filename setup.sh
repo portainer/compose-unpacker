3eaa9188f99922009155f4d47d75b2d0c908ef85
sunportainer <eric.sun@portainer.io>
2022-06-27 18:12:38 +0800

    remove kubectrl version

--- setup.sh
+++ setup.sh
@@ -1,14 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
 IFS=$'\n\t'
-
 PLATFORM=${1:-"linux"}
 ARCH=${2:-"amd64"}
-
 DOCKER_VERSION="v20.10.9"
 DOCKER_COMPOSE_VERSION="v2.5.1"
-KUBECTL_VERSION="v1.24.1"
-
 mkdir -p dist/
 
 /usr/bin/env bash ./build/download_docker_binary.sh "$PLATFORM" "$ARCH" "$DOCKER_VERSION"
