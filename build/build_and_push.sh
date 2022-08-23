4766ba6263a4ae7cc48757910ff996e017493181
Matt Hook <hookenz@gmail.com>
2022-08-23 13:47:54 +1200

    rework to support windows

--- build/build_and_push.sh
+++ build/build_and_push.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 set -e
+set +x
 
 : ${REPO:=$1/compose-unpacker}
 : ${TAG:=$2}
 
+BUILDS=('linux/amd64' 'linux/arm64' 'linux/arm' 'windows/amd64')
+
+
 docker_image_build_and_push()
 {
-  arch=${1?required}
-  os=${2?required}
+  os=${1?required}
+  arch=${2?required}
   repo=${3?required}
   tag=${4?required}
 
@@ -18,8 +22,7 @@ docker_image_build_and_push()
       build_args="--build-arg OSVERSION=1809"
   fi
 
-  docker buildx build -o type=docker -f ${dockerfile} ${build_args} --platform ${os}/${arch} -t ${repo}:${tag}-${os}-${arch} .
-  docker image push ${repo}:${tag}-${os}-${arch}
+  docker buildx build --push -f ${dockerfile} ${build_args} --platform ${os}/${arch} --tag ${repo}:${tag}-${os}-${arch} .
 }
 
 docker_manifest_create_and_push()
@@ -27,47 +30,32 @@ docker_manifest_create_and_push()
   repo=${1?required}
   tag=${2?required}
 
-  images=$(docker image ls "${repo}:${tag}*" --format '{{.Repository}}:{{.Tag}}')
+  for build in "${BUILDS[@]}"
+  do
+    IFS='/' read -ra build_parts <<< "$build"
+    os=${build_parts[0]}
+    arch=${build_parts[1]}
 
-  docker manifest create --amend ${repo}:${tag} $images
-  for img in $images; do    
-    if [[ "$img" == *"win"* ]]; then
-      os="windows"
-    else
-      os="linux"
-    fi
-
-    case ${img} in
-      *"amd64"*)
-        arch="amd64";;
-      *"arm64"*)
-        arch="arm64";;
-      *"arm"*)
-        arch="arm32";;
-      *)
-        continue;;
-    esac
-
+    image="${repo}:${tag}-${os}-${arch}"
+    docker manifest create --amend ${repo}:${tag} $images
     docker manifest annotate ${repo}:${tag} ${img} --os ${os} --arch ${arch}
-  done
+  done  
   
   docker manifest push ${repo}:${tag}
 }
 
-make clean
-make PLATFORM=linux ARCH=amd64
-docker_image_build_and_push amd64 linux ${REPO} ${TAG} 
 
-make clean
-make PLATFORM=linux ARCH=arm64
-docker_image_build_and_push arm64 linux ${REPO} ${TAG} 
+for build in "${BUILDS[@]}"
+do
+  echo "Creating build $build ..."
+  IFS='/' read -ra build_parts <<< "$build"
+  os=${build_parts[0]}
+  arch=${build_parts[1]}
 
-make clean
-make PLATFORM=linux ARCH=arm
-docker_image_build_and_push arm linux ${REPO} ${TAG} 
+  make clean
+  make PLATFORM=${os} ARCH=${arch}
+  docker_image_build_and_push ${os} ${arch} ${REPO} ${TAG} 
+done
 
-make clean
-make PLATFORM=windows ARCH=amd64
-docker_image_build_and_push amd64 windows ${REPO} ${TAG} 
 
 docker_manifest_create_and_push ${REPO} ${TAG}
