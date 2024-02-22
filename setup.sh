19196c848d39b91ff7ef005648b6c25c3382d919
hookenz <hookenz@gmail.com>
2024-02-23 08:15:05 +1300

    update binary versions to match portainer api server

--- setup.sh
+++ setup.sh
@@ -3,8 +3,8 @@ set -euo pipefail
 IFS=$'\n\t'
 PLATFORM=${1:-"linux"}
 ARCH=${2:-"amd64"}
-DOCKER_VERSION="v20.10.9"
-DOCKER_COMPOSE_VERSION="v2.5.1"
+DOCKER_VERSION="v24.0.7"
+DOCKER_COMPOSE_VERSION="v2.23.3"
 mkdir -p dist/
 
 /usr/bin/env bash ./build/download_docker_binary.sh "$PLATFORM" "$ARCH" "$DOCKER_VERSION"
