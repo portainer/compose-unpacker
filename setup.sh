41e0e13f5396287b5bc047ae77d1dcc381acacbe
stevensbkang <skan070@gmail.com>
2025-03-14 14:33:10 +1300

    fix: multiple cves

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 IFS=$'\n\t'
 PLATFORM=${1:-"linux"}
 ARCH=${2:-"amd64"}
-DOCKER_VERSION="v27.1.2"
+DOCKER_VERSION="v27.5.1"
 mkdir -p dist/
 
 /usr/bin/env bash ./build/download_docker_binary.sh "$PLATFORM" "$ARCH" "$DOCKER_VERSION"
